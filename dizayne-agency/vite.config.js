d901c41529cebcbdaaa76f0e4e21f204b8e1e9a6
zahraaAed <zahraaalaaeddine94@gmail.com>
2024-05-07 14:46:25 +0300

    react web

---
+++ dizayne-agency/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  alias: {
+    '@fortawesome/free-solid-svg-icons': '@fortawesome/free-solid-svg-icons', // Ensure correct aliasing
+  },
+})
+
+
